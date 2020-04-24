d50007c6d239c80958a682d9f5e88a761421d15c
Collins Eze <ezeamaka2@gmail.com>
2020-04-24 02:21:07 +0100

    commit to master

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("Stage two, dependencies setup");
+  res.send("Stage two, dependencies setdup");
   next();
 });
 
