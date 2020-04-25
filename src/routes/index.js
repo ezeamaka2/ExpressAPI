09a3017687d10611449bef7180a84900e4b9ca7c
Collins Eze <ezeamaka2@gmail.com>
2020-04-26 00:03:43 +0100

    added middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,12 @@ import express from "express";
 
 import { indexPage } from "../controllers";
 import { messagesPage, addMessage } from "../controllers/messages";
+import { modifyMessage } from "../middleware";
 
 const indexRouter = express.Router();
 
 indexRouter.get("/", indexPage);
 indexRouter.get("/messages", messagesPage);
-indexRouter.post("/messages", addMessage);
+indexRouter.post("/messages", modifyMessage, addMessage);
 
 export default indexRouter;
